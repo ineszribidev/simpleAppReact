310143aaeab013df3ea32290b3677770ce2e0f21
ines <ineszribi89@email.com>
2021-01-11 23:06:11 +0100

    simplephrase

---
+++ src/components/Testcompo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Testcompo extends Component {
+  render() {
+    return (
+      <div className="testcompo">
+        <div>Nom:Alex</div>
+        <div>Prenom: zribi</div>
+        <div>statut:developper</div>
+      </div>
+    );
+  }
+}
+
+export default Testcompo;
